4646024f7089a1cb837d4ae18434dc0c28ac7cd9
banthita-tho <banthita.tho@ku.th>
2026-07-09 17:51:04 +0700

    Add files via upload

--- src/lab/Specs.java
+++ src/lab/Specs.java
@@ -17,6 +17,13 @@ package lab;
 public class Specs {
 
     // TODO ข้อ 4: เขียน JavaDoc ให้เมธอดนี้
+    /**
+     * เช็คค่าของ arr ว่าตรงกับค่า key มั้ยถ้าตรงจะ -1
+     * @param arr not null
+     * @param key ค่าที่ต้องการหา
+     * @return -1 ถ้า arr ตรงกับ key
+     * @throws IllegalArg arr == null
+     */
     public static int firstIndexOf(int[] arr, int key) {
         if (arr == null) throw new IllegalArgumentException("arr must not be null");
         for (int i = 0; i < arr.length; i++)
@@ -26,9 +33,10 @@ public class Specs {
 
     // TODO ข้อ 5: เขียน JavaDoc ให้เมธอดนี้
     /**
-     * 
-     * @param radius
-     * @return
+     * คำนวนค่าพื้นที่วงกลม
+     * @param radius ต้องมากกว่า 0
+     * @throws IllegalArg ถ้า radius <= 0
+     * @return หาค่าพื้นที่วงกลม PI * radius * radius
      */
     public static double circleArea(double radius) {
         if (radius < 0) throw new IllegalArgumentException("radius must be >= 0");
@@ -36,8 +44,13 @@ public class Specs {
     }
 
     // TODO ข้อ 6: เขียน JavaDoc ให้เมธอดนี้
-    //   (เมธอดนี้ตัดช่องว่างหน้า-หลัง แล้วแปลงเป็นตัวพิมพ์เล็ก;
     //    input null ให้คืน string ว่าง)
+
+    /**
+     * เมธอดนี้ตัดช่องว่างหน้า-หลัง แล้วแปลงเป็นตัวพิมพ์เล็ก;
+     * @param s not null
+     * @return ข้อความเป็นตัวพิมพ์เล็กไม่เว้นช่องหน้าหลัง
+     */
     public static String normalize(String s) {
         if (s == null) return "";
         return s.trim().toLowerCase();
